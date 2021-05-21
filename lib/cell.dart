5021f4022ad66c129a86b7c35743aa39337b4b41
nemilya <nemilya@gmail.com>
2021-05-21 21:15:31 +0500

    small refactorign

--- lib/cell.dart
+++ lib/cell.dart
@@ -4,11 +4,17 @@ class Cell{
   static const LIFE  = '*';
 
   static String next_generation_state(String cell_state, int neighbors_cnt){
-    if (cell_state == Cell.EMPTY){
-      return neighbors_cnt == 3 ? Cell.LIFE : Cell.EMPTY; 
-    }
-    else if (cell_state == Cell.LIFE){
-      return (neighbors_cnt == 3 || neighbors_cnt == 2) ? Cell.LIFE : Cell.EMPTY; 
+    switch(cell_state){
+
+      case Cell.EMPTY:
+        return
+          neighbors_cnt == 3 ? 
+            Cell.LIFE : Cell.EMPTY;
+ 
+      case Cell.LIFE:
+        return 
+          (neighbors_cnt == 3 || neighbors_cnt == 2) ? 
+            Cell.LIFE : Cell.EMPTY; 
     }
   }
 }
