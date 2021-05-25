4fd5d96e76c155b009793d5c3b51419b0f92ce61
nemilya <nemilya@gmail.com>
2021-05-25 23:31:41 +0500

    text indent

--- lib/game_life.dart
+++ lib/game_life.dart
@@ -16,13 +16,14 @@ class GameLife{
     return generation.as_ascii();
   }
 
+  // generate next generation
   void step(){
     World next_generation = new World();
     next_generation.init(rows: generation.rowsCnt(), cols: generation.colsCnt());
     for (var r = 0; r < generation.rowsCnt(); r++){
       for (var c = 0; c < generation.colsCnt(); c++){
-        var cur_cell_state = generation.get_cell_at(r, c);
-        var n_cnt = generation.neighbors_cnt_at(r, c);
+        var cur_cell_state  = generation.get_cell_at(r, c);
+        var n_cnt           = generation.neighbors_cnt_at(r, c);
         var next_cell_state = Cell.next_generation_state(cur_cell_state, n_cnt);
         next_generation.set_cell(r, c, next_cell_state);
       }
